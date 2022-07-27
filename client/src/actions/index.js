586988d765ce35cd303cda6be62acba05c3ea774
Haris Pasic <harispasic@ymail.com>
2022-07-27 12:12:33 +0200

    VideoStreamer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -23,46 +23,66 @@ export const signOut = () => {
 };
 
 export const fetchStreams = () => async dispatch => {
-  const res = await streams.get("/streams");
-  dispatch({
-    type: FETCH_LIST_STREAMS,
-    payload: res.data,
-  });
+  try {
+    const res = await streams.get("/streams");
+    dispatch({
+      type: FETCH_LIST_STREAMS,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const createStream = formValues => async (dispatch, getState) => {
-  const { userId } = getState().auth;
-  const res = await streams.post("/streams", { ...formValues, userId });
-  dispatch({
-    type: CREATE_STREAM,
-    payload: res.data,
-  });
+  try {
+    const { userId } = getState().auth;
+    const res = await streams.post("/streams", { ...formValues, userId });
+    dispatch({
+      type: CREATE_STREAM,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const editStream = (id, formValues) => {
-  return async dispatch => {
-    const res = await streams.patch(`/streams/${id}`, formValues);
-    dispatch({
-      type: EDIT_STREAM,
-      payload: res.data,
-    });
-  };
+  try {
+    return async dispatch => {
+      const res = await streams.patch(`/streams/${id}`, formValues);
+      dispatch({
+        type: EDIT_STREAM,
+        payload: res.data,
+      });
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const fetchStream = id => async dispatch => {
-  const res = await streams.get(`/streams/${id}`);
-  dispatch({
-    type: FETCH_STREAM,
-    payload: res.data,
-  });
+  try {
+    const res = await streams.get(`/streams/${id}`);
+    dispatch({
+      type: FETCH_STREAM,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const deleteStream = id => {
-  return async dispatch => {
-    await streams.delete(`/streams/${id}`);
-    dispatch({
-      type: DELETE_STREAM,
-      payload: id,
-    });
-  };
+  try {
+    return async dispatch => {
+      await streams.delete(`/streams/${id}`);
+      dispatch({
+        type: DELETE_STREAM,
+        payload: id,
+      });
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
